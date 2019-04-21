86a5fe8096b91feebe2aa8f670947a9949df0adf
fserlut_home <zakharovvilya@gmail.com>
2019-04-21 14:21:43 +0300

    upd 20.04

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: fserlut <fserlut@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/08 05:59:32 by fserlut           #+#    #+#             */
-/*   Updated: 2019/04/19 16:42:21 by fserlut          ###   ########.fr       */
+/*   Updated: 2019/04/20 01:58:07 by fserlut          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@ int	ft_atoi(const char *str)
 {
 	int i;
 	int fg;
-	int nb;
+	long long int nb;
 
 	nb = 0;
 	i = 0;
--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: fserlut <fserlut@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/19 19:43:21 by fserlut           #+#    #+#             */
-/*   Updated: 2019/04/19 20:28:01 by fserlut          ###   ########.fr       */
+/*   Updated: 2019/04/20 20:24:40 by fserlut          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,74 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/**
+int		ft_count_numb(int n)
+{
+	int i;
+
+	i = 0;
+	if (n == 0)
+		i++;
+	else if (n < 0)
+		i++;
+	else
+	{
+		while (n > 0)
+		{
+			n /= 10;
+			i++;
+		}
+	}
+	return (i);
+}
+
+void	ft_strrev(char *s)
+{
+	int i;
+	int	i_end;
+	char c;
+
+	i = 0;
+	i_end = ft_strlen(s) - 1;
+	while (i < i_end)
+	{
+		c = s[i];
+		s[i++] = s[i_end];
+		s[i_end--] = c;
+	}
+}
+
 char    *ft_itoa(int n)
 {
-	return ("xx");
+	char *s;
+	int i;
+	int fg;
+
+	fg = 0;
+	i = 0;
+	s = ft_strnew(ft_count_numb(n));	
+	if (n < 0)
+	{
+		n = n * -1;
+		fg = 1;
+	}
+	if (n == 0)
+		s[i] = '0';
+	while (n > 0)
+	{
+		s[i++] = (n % 10) + '0';
+		n /= 10;
+	}
+	if (fg == 1)
+		s[i++] = '-';
+	ft_strrev(s);
+	return (s);
 }
 
 int		main()
 {
-
 	int x;
-	int q;
 
-	q = 0;
-	x = 22;
-	while(x)
-	{
-		x++;
-		q++;
-	}
-	printf("%d\n", q);
+	x = 0;
+	printf("%s\n", ft_itoa(x));
 	return (0);
 }
-**/
